fc07c7eccdc6f0c113b274807d85e0cd68b800f2
tekajurt <danielalejandro.hurtado@alumnos.ulagos.cl>
2021-04-26 23:02:05 -0400

    se crea un componente Template, donde se guarda el cuerpo de la página, y luego  desde ahí se llama al resto de componentes una vez dentro del folder componentes, podría ser mejor, pero por ahora ahí está la wea xd

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,55 +1,7 @@
-import Template from './componentes/template'
-import About from './componentes/about'
-import Users from './componentes/users'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Template from './componentes/template';
 function App() {
   return (
-    <Router>
-      <div>
-        <div className="jumbotron">
-          <h1 className="display-4 text-center">Header</h1>
-        </div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="/">Navbar</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              {/*
-                links que son etiquetas de react para el redireccionado (routing)
-              */}
-              <Link className="nav-item nav-link" activeStyle={{ "background-color": '#bdc3c9!important' }} to="/">Home</Link>
-              <Link className="nav-item nav-link" to="/about">About</Link>
-              <Link className="nav-item nav-link" to="/users">Users</Link>
-            </div>
-          </div>
-        </nav>
-        {/*
-          switch es una etiqueta jsx de react que permite gestionar el routing, notece la diferencia que la etiqueta de arriba es Router, aca para
-          cada ruta es "Route", donde como parametro se le agrega el path="" para demostrar lo que sucede luego de el / (incluyendo el /)
-        */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/" exact>
-            {/*
-            el exact es para renderizar solo cuando el / esta solo, sino se asume arriba que, por ejemplo,  / y users en la misma vez
-          */}
-            <Template />
-          </Route>
-        </Switch>
-      </div>
-    </Router >
+    <Template />
   );
 }
 export default App;
--- routing/src/componentes/template.js
+++ routing/src/componentes/template.js
@@ -1,6 +1,56 @@
 import React from 'react'
+import Home from './home'
+import About from './about'
+import Users from './users'
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 export default function template() {
     return (
-        <h1>Home page</h1>
+        <Router>
+            <div>
+                <div className="jumbotron">
+                    <h1 className="display-4 text-center">Header</h1>
+                </div>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="/">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            {/*
+                links que son etiquetas de react para el redireccionado (routing) que obtienen el ovjeto (si wn, ovjeto) del folder /componentes
+              */}
+                            <Link className="nav-item nav-link" to="/">Home</Link>
+                            <Link className="nav-item nav-link" to="/about">About</Link>
+                            <Link className="nav-item nav-link" to="/users">Users</Link>
+                        </div>
+                    </div>
+                </nav>
+                {/*
+          switch es una etiqueta jsx de react que permite gestionar el routing, notece la diferencia que la etiqueta de arriba es Router, aca para
+          cada ruta es "Route", donde como parametro se le agrega el path="" para demostrar lo que sucede luego de el / (incluyendo el /)
+        */}
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/users">
+                        <Users />
+                    </Route>
+                    <Route path="/" exact>
+                        {/*
+            el exact es para renderizar solo cuando el / esta solo, sino se asume arriba que, por ejemplo,  / y users en la misma vez
+          */}
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
